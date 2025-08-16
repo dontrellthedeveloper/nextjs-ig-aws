d1cac6f7dea86920bdc15a8bf4d2e92c54471ebd
dontrellthedeveloper <dontrellknight@gmail.com>
2025-08-16 13:30:33 -0700

    updated routing in navigation bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -124,30 +124,30 @@ const Navbar: FC = () => {
             </Link>
 
             {/* Messages */}
-            <button className="p-2 hover:bg-gray-100 rounded-lg transition-colors duration-200 relative group">
+            <Link href="/messages" className="p-2 hover:bg-gray-100 rounded-lg transition-colors duration-200 relative group">
               <MessagesIcon />
               <span className="absolute -top-1 -right-1 w-5 h-5 bg-red-500 text-white text-xs rounded-full flex items-center justify-center font-medium">
                 3
               </span>
-            </button>
+            </Link>
 
             {/* Create Post */}
-            <button className="p-2 hover:bg-gray-100 rounded-lg transition-colors duration-200 group">
+            <Link href="/create" className="p-2 hover:bg-gray-100 rounded-lg transition-colors duration-200 group">
               <UploadIcon />
-            </button>
+            </Link>
 
             {/* Explore */}
-            <button className="p-2 hover:bg-gray-100 rounded-lg transition-colors duration-200 group">
+            <Link href="/explore" className="p-2 hover:bg-gray-100 rounded-lg transition-colors duration-200 group">
               <svg className="w-6 h-6 text-gray-700 group-hover:text-gray-900 transition-colors" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
               </svg>
-            </button>
+            </Link>
 
             {/* Notifications */}
-            <button className="p-2 hover:bg-gray-100 rounded-lg transition-colors duration-200 relative group">
+            <Link href="/notifications" className="p-2 hover:bg-gray-100 rounded-lg transition-colors duration-200 relative group">
               <NotificationsIcon />
               <span className="absolute -top-1 -right-1 w-2 h-2 bg-red-500 rounded-full"></span>
-            </button>
+            </Link>
 
             {/* Profile Dropdown */}
             <div className="relative" ref={dropdownRef}>
